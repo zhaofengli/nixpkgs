f3968e9ca79132a84af897e2310fc0ffaf8134cb
Zhaofeng Li <hello@zhaofeng.li>
2021-04-30 00:59:16 +0000

    phosh: init at 0.10.2
    Co-authored-by: Blaž Hrastnik <blaz@mxxn.io>
    Co-authored-by: Jan Tojnar <jtojnar@gmail.com>
    Co-authored-by: Jordi Masip <jordi@masip.cat>

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -7536,6 +7536,8 @@ in
 
   pim6sd = callPackage ../servers/pim6sd { };
 
+  phosh = callPackage ../applications/window-managers/phosh { };
+
   pinentry = libsForQt5.callPackage ../tools/security/pinentry {
     libcap = if stdenv.isDarwin then null else libcap;
   };
