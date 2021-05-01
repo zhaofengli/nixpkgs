c4a37200754b48e28eaa6e30ef026a888dddf30e
Zhaofeng Li <hello@zhaofeng.li>
2021-05-01 03:27:33 +0000

    firefox-pmos-mobile: init at -pmos-2.0.1

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -24067,6 +24067,8 @@ in
     desktopName = "Firefox DevEdition";
   };
 
+  firefox-pmos-mobile = callPackage ../applications/networking/browsers/firefox-pmos-mobile { };
+
   flac = callPackage ../applications/audio/flac { };
 
   redoflacs = callPackage ../applications/audio/redoflacs { };
