8ffe03b198824a1c1dc434921906eb684768a8c3
Zhaofeng Li <hello@zhaofeng.li>
2021-07-06 20:37:12 -0700

    libunwind: 0.4.0 -> 0.5.0
    Also adds patches for RISC-V support which has been merged into master.

--- pkgs/development/libraries/libunwind/default.nix
+++ pkgs/development/libraries/libunwind/default.nix
@@ -1,15 +1,25 @@
-{ stdenv, lib, fetchurl, autoreconfHook, xz, coreutils }:
+{ stdenv, lib, fetchurl, fetchpatch, autoreconfHook, xz, coreutils }:
 
 stdenv.mkDerivation rec {
   pname = "libunwind";
-  version = "1.4.0";
+  version = "1.5.0";
 
   src = fetchurl {
     url = "mirror://savannah/libunwind/${pname}-${version}.tar.gz";
-    sha256 = "0dc46flppifrv2z0mrdqi60165ghxm1wk0g47vcbyzjdplqwjnfz";
+    sha256 = "05qhzcg1xag3l5m3c805np6k342gc0f3g087b7g16jidv59pccwh";
   };
 
-  patches = [ ./backtrace-only-with-glibc.patch ];
+  patches = [
+    # RISC-V support
+    (fetchpatch {
+      url = "https://github.com/zhaofengli/libunwind/commit/2abe45510f57e62651e3980d2a3c972c678a2eab.patch";
+      sha256 = "1jrlr7wnr989ppci1j2ldmwpz1v43lazks2hsic4v727gq7qpww9";
+    })
+    (fetchpatch {
+      url = "https://github.com/zhaofengli/libunwind/commit/a2e53182ae351f5075796b2faef767b17f31f510.patch";
+      sha256 = "15qwkmikz3k4h526prcsg68xl7k0yx4lc0x4vrmi0a71p5gdsy5a";
+    })
+  ];
 
   postPatch = lib.optionalString stdenv.hostPlatform.isMusl ''
     substituteInPlace configure.ac --replace "-lgcc_s" "-lgcc_eh"
@@ -38,7 +48,6 @@ stdenv.mkDerivation rec {
     description = "A portable and efficient API to determine the call-chain of a program";
     maintainers = with maintainers; [ orivej ];
     platforms = platforms.linux;
-    badPlatforms = [ "riscv32-linux" "riscv64-linux" ];
     license = licenses.mit;
   };
 }
