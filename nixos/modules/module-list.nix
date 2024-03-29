30643b0aaa5b679ebfc7d1cf6257a724f0843c9e
nu-nu-ko <153512689+nu-nu-ko@users.noreply.github.com>
2024-03-29 07:56:46 -0600

    nixos/qbittorrent: init service module

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -1266,6 +1266,7 @@
   ./services/torrent/rtorrent.nix
   ./services/torrent/transmission.nix
   ./services/torrent/torrentstream.nix
+  ./services/torrent/qbittorrent.nix
   ./services/tracing/tempo.nix
   ./services/ttys/getty.nix
   ./services/ttys/gpm.nix
