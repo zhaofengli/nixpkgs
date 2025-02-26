4485e883cba49ea2323d13fc68afac702e21bdce
Zhaofeng Li <hello@zhaofeng.li>
2025-02-26 13:10:45 -0700

    [downstream] secretive: drop

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -5014,8 +5014,6 @@ with pkgs;
 
   scfbuild = python3.pkgs.callPackage ../tools/misc/scfbuild { };
 
-  secretive = callPackage ../os-specific/darwin/secretive { };
-
   securefs = darwin.apple_sdk_11_0.callPackage ../tools/filesystems/securefs { };
 
   segger-jlink-headless = callPackage ../by-name/se/segger-jlink/package.nix { headless = true; };
