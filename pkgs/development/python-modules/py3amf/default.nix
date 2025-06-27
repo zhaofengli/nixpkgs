5296f539e60c00672c590556ac1bc9445ce80db5
Zhaofeng Li <hello@zhaofeng.li>
2025-06-26 23:29:28 -0600

    python3Packages.py3amf: init at 0.8.11

---
+++ pkgs/development/python-modules/py3amf/default.nix
@@ -0,0 +1,42 @@
+{
+  lib,
+  buildPythonPackage,
+  fetchFromGitHub,
+  setuptools,
+  wheel,
+  defusedxml,
+}:
+
+buildPythonPackage rec {
+  pname = "py3amf";
+  version = "0.8.11";
+  pyproject = true;
+
+  src = fetchFromGitHub {
+    owner = "StdCarrot";
+    repo = "Py3AMF";
+    rev = "v${version}";
+    hash = "sha256-9zuHh5+ggIjv1LcjpBNHy2yh09KsFpxUdGrtKGm94Zg=";
+  };
+
+  build-system = [
+    setuptools
+    wheel
+  ];
+
+  dependencies = [
+    defusedxml
+  ];
+
+  pythonImportsCheck = [
+    "pyamf"
+  ];
+
+  meta = {
+    description = "Action Message Format (AMF) support for Python 3";
+    homepage = "https://github.com/StdCarrot/Py3AMF";
+    changelog = "https://github.com/StdCarrot/Py3AMF/blob/${src.rev}/CHANGES.txt";
+    license = lib.licenses.mit;
+    maintainers = with lib.maintainers; [ zhaofengli ];
+  };
+}
